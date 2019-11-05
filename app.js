b32d090e7b38e8c25b5a4e5ee76c54fabff6f397
Martin-Mutua <Martin.n.mutua@gmail.com>
2019-11-05 03:52:34 +0300

    express and node setting up.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();
+
+//to handle CORS errors.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/recipes', (req, res, next) => {
+    
+});
+
+module.exports = app;
